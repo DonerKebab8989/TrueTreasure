449ba286dbdd6640ef83ce33ecf0f8f6843494d0
DonerKebab8989 <24e1808j@shinshu-u.ac.jp>
2025-12-05 09:44:44 +0900

    1

--- True Treasure.py
+++ True Treasure.py
@@ -1,8 +1,16 @@
 import streamlit as st
 
 story1 = st.Page("story1.py", title="ストーリー１")
+story2 = st.Page("story2.py", title="ストーリー２")
+story3 = st.Page("story3.py", title="ストーリー３")
+story4 = st.Page("story4.py", title="ストーリー４")
+story5 = st.Page("story5.py", title="ストーリー５")
 
 st.title("True Treasure")
 
-name = st.text_input("Tell your name")
+user_name = st.text_input("Tell your name", "")
 
+if user_name:
+    st.write(f"Hello, **{user_name}**.")
+
+st.page_link("story1.py", label="New Game", icon="🆕")
