968499eeffca3bc9010da7ade09a827099c4c254
DonerKebab8989 <24e1808j@shinshu-u.ac.jp>
2025-12-05 13:48:33 +0900

    2

--- story1.py
+++ story1.py
@@ -1,3 +1,10 @@
 import streamlit as st
 
-st.title("story1")
+st.title("True Tresure")
+
+user_name = st.text_input("Tell your name", "")
+
+if user_name:
+    st.write(f"Hello, **{user_name}**.")
+
+st.page_link("story2.py", label="New Game")
